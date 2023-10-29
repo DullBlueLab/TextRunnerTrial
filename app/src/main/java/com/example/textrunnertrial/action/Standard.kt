39cc716c513ba542f16640957f788abf61d856cf
DullBlueLab <142051314+DullBlueLab@users.noreply.github.com>
2023-10-29 11:07:29 +0900

    set ic_launcher change package name

--- app/src/main/java/com/example/textrunnertrial/action/Standard.kt
+++
@@ -1,9 +0,0 @@
-package com.example.textrunnertrial.action
-
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-
-class Standard {
-
-}
